68d51870ef08d9504e4fde31e5fadaba98f2436d
Wubster <thetaylorstephens@hotmail.com>
2016-04-20 20:17:31 -0600

    Added files via upload
    new

--- send_form_email.php
+++ send_form_email.php
@@ -1,5 +1,5 @@
 <?php
-echo "Successfully Sent! - ";
+echo "Message - ";
 if(isset($_POST['email'])) {
      
     $email_to = "thetaylorstephens@hotmail.com";
@@ -9,7 +9,7 @@ if(isset($_POST['email'])) {
      
     function died($error) {
         // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "I am very sorry, but there were error(s) found with the form you submitted. ";
         echo "These errors appear below.<br /><br />";
         echo $error."<br /><br />";
         echo "Please go back and fix these errors.<br /><br />";
